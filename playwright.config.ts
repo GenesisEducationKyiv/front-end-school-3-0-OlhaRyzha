0f7908c814aa208780249bee4d1b834e78ebc37c
OlhaRyzha <104225380+OlhaRyzha@users.noreply.github.com>
2025-07-04 23:12:33 +0300

    updated ci.yml

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,8 +19,17 @@ export default defineConfig({
     screenshot: 'on',
   },
   projects: [
-    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
-    { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
-    { name: 'webkit', use: { ...devices['Desktop Safari'] } },
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
   ],
 });
