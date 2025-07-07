f7707b579fc71418508de4170bcb59d44fb4d615
OlhaRyzha <104225380+OlhaRyzha@users.noreply.github.com>
2025-07-07 18:58:36 +0300

    updated pagination component

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -3,7 +3,10 @@ import { ChevronLeft, ChevronRight, MoreHorizontal } from '@/components/icons';
 import { cn } from '@/lib/utils';
 import { ButtonProps, buttonVariants } from '@/components/ui/button';
 
-const Pagination = ({ className, ...props }: React.ComponentProps<'nav'>) => (
+export const Pagination = ({
+  className,
+  ...props
+}: React.ComponentProps<'nav'>) => (
   <nav
     data-testid='pagination'
     role='navigation'
@@ -14,19 +17,19 @@ const Pagination = ({ className, ...props }: React.ComponentProps<'nav'>) => (
 );
 Pagination.displayName = 'Pagination';
 
-const PaginationContent = React.forwardRef<
+export const PaginationContent = React.forwardRef<
   HTMLUListElement,
   React.ComponentProps<'ul'>
 >(({ className, ...props }, ref) => (
   <ul
     ref={ref}
-    className={cn('flex flex-row items-center gap-1', className)}
+    className={cn('flex flex-row items-center gap-2', className)}
     {...props}
   />
 ));
 PaginationContent.displayName = 'PaginationContent';
 
-const PaginationItem = React.forwardRef<
+export const PaginationItem = React.forwardRef<
   HTMLLIElement,
   React.ComponentProps<'li'>
 >(({ className, ...props }, ref) => (
@@ -44,80 +47,84 @@ type PaginationLinkProps = {
 } & Pick<ButtonProps, 'size'> &
   React.ComponentProps<'a'>;
 
-const PaginationLink = ({
-  className,
+export const PaginationLink = ({
   isActive,
   disabled,
   size = 'icon',
+  className,
   ...props
-}: PaginationLinkProps) => (
-  <a
-    aria-current={isActive ? 'page' : undefined}
-    className={cn(
-      buttonVariants({
-        variant: isActive ? 'outline' : 'ghost',
-        size,
-      }),
-      disabled && 'opacity-50 cursor-not-allowed',
-      className
-    )}
-    {...props}
-  />
-);
+}: PaginationLinkProps) => {
+  const base = buttonVariants({
+    variant: isActive ? 'default' : 'ghost',
+    size,
+  });
+  const activeClasses = isActive
+    ? ''
+    : 'text-foreground hover:bg-accent hover:text-accent-foreground';
+  const disabledClasses = disabled
+    ? 'cursor-not-allowed disabled:text-gray-600'
+    : '';
+  return (
+    <a
+      aria-current={isActive ? 'page' : undefined}
+      className={cn(
+        base,
+        'min-h-[20px] min-w-[20px] flex items-center justify-center',
+        activeClasses,
+        disabledClasses,
+        className
+      )}
+      {...props}
+    />
+  );
+};
 PaginationLink.displayName = 'PaginationLink';
 
-const PaginationPrevious = ({
-  className,
+export const PaginationPrevious = ({
   disabled,
   ...props
 }: React.ComponentProps<typeof PaginationLink>) => (
   <PaginationLink
     aria-label='Go to previous page'
     size='default'
-    className={cn('gap-1 pl-2.5', className)}
     disabled={disabled}
+    className='gap-1 pl-2.5'
     {...props}>
     <ChevronLeft className='h-4 w-4' />
     <span>Prev</span>
   </PaginationLink>
 );
+PaginationPrevious.displayName = 'PaginationPrevious';
 
-const PaginationNext = ({
-  className,
+export const PaginationNext = ({
   disabled,
   ...props
 }: React.ComponentProps<typeof PaginationLink>) => (
   <PaginationLink
     aria-label='Go to next page'
     size='default'
-    className={cn('gap-1 pr-2.5', className)}
     disabled={disabled}
+    className='gap-1 pr-2.5'
     {...props}>
     <span>Next</span>
     <ChevronRight className='h-4 w-4' />
   </PaginationLink>
 );
+PaginationNext.displayName = 'PaginationNext';
 
-const PaginationEllipsis = ({
+export const PaginationEllipsis = ({
   className,
   ...props
 }: React.ComponentProps<'span'>) => (
   <span
     aria-hidden
-    className={cn('flex h-9 w-9 items-center justify-center', className)}
+    className={cn(
+      'flex items-center justify-center min-h-[20px] min-w-[20px] text-foreground',
+      className
+    )}
     {...props}>
     <MoreHorizontal className='h-4 w-4' />
     <span className='sr-only'>More pages</span>
   </span>
 );
 PaginationEllipsis.displayName = 'PaginationEllipsis';
-
-export {
-  Pagination,
-  PaginationContent,
-  PaginationLink,
-  PaginationItem,
-  PaginationPrevious,
-  PaginationNext,
-  PaginationEllipsis,
-};
